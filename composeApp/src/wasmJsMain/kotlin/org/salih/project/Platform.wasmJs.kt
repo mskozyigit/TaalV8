8141498639e32b58fb4f2cbf76fb059b9de38480
Your Name <you@example.com>
2026-05-07 23:47:55 +0200

    Update greeting to 'merhana dunya'

---
+++ composeApp/src/wasmJsMain/kotlin/org/salih/project/Platform.wasmJs.kt
@@ -0,0 +1,7 @@
+package org.salih.project
+
+class WasmPlatform: Platform {
+    override val name: String = "Web with Kotlin/Wasm"
+}
+
+actual fun getPlatform(): Platform = WasmPlatform()
---
+++ composeApp/src/webMain/kotlin/org/salih/project/Greeting.kt
@@ -0,0 +1,10 @@
+package org.salih.project
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        // Return the requested message in Turkish
+        return "merhana dunya"
+    }
+}
